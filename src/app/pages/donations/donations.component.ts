db2bfdb1cb80e2b51eb1e678495461027529b61a
Alexander <alexander.gallol1020@gmail.com>
2023-04-23 22:35:27 -0500

    structure donations page with logic, styles and html

--- src/app/pages/donations/donations.component.ts
+++ src/app/pages/donations/donations.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+
 
 @Component({
   selector: 'app-donations',
   templateUrl: './donations.component.html',
   styleUrls: ['./donations.component.scss']
 })
-export class DonationsComponent {
+export class DonationsComponent implements OnInit {
 
+  faWhatsapp = faWhatsapp;
+
+  logo = '../assets/image/logos/logo-nuevo-filantropia.png';
+  title = 'Transforma vidas con tus aportes';
+  footer = 'Donar es muy sencillo solo debes seguir los pasos tu donación será rápida y 100% segura'
+
+  step: any = 1;
+  stepContact: any;
+
+  ngOnInit(): void {
+  }
+
+  next() {
+    this.step = this.step + 1
+  }
+
+  back() {
+    this.step = this.step - 1
+  }
+
+  nextContact() {
+    this.stepContact = 1
+  }
+
+  backContact() {
+  this.step = 1
+  this.stepContact = 0
+  }
 }
